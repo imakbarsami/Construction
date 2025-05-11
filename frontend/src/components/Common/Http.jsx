e5dbbe112008a5c1eb675c363e405b96a40d9eae
Akbar Sami <109865799+imakbarsami@users.noreply.github.com>
2025-05-11 22:00:08 +0600

    actual url added

--- frontend/src/components/Common/Http.jsx
+++ frontend/src/components/Common/Http.jsx
@@ -1,6 +1,9 @@
 
-export const apiUrl = 'http://localhost:8000/api/'
-export const fileUrl = 'http://localhost:8000/'
+// export const apiUrl = 'http://localhost:8000/api/'
+// export const fileUrl = 'http://localhost:8000/'
+
+export const apiUrl = 'http://elevateworks.wuaze.com/api/'
+export const fileUrl = 'http://elevateworks.wuaze.com/'
 
 export const token = () => {
     const userInfo = localStorage.getItem('userInfo')
